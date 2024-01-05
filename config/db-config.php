20f3ddb4f853a8f9b99bb67025d33cefa6e173ac
Arturo Santana <santana.inffo@gmail.com>
2024-01-05 19:38:35 +0000

    🔑 add: config files

---
+++ config/db-config.php
@@ -0,0 +1,8 @@
+<?php
+
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_NAME', 'osw_inv' );        // Set database name
+
+?>
---
+++ config/load.php
@@ -0,0 +1,22 @@
+<?php
+// -----------------------------------------------------------------------
+// DEFINE SEPERATOR ALIASES
+// -----------------------------------------------------------------------
+define("URL_SEPARATOR", '/');
+
+define("DS", DIRECTORY_SEPARATOR);
+
+// -----------------------------------------------------------------------
+// DEFINE ROOT PATHS
+// -----------------------------------------------------------------------
+defined('SITE_ROOT')? null: define('SITE_ROOT', realpath(dirname(__FILE__)));
+define("LIB_PATH_INC", SITE_ROOT.DS);
+
+require_once(LIB_PATH_INC.'db-config.php');
+require_once(LIB_PATH_INC.'functions.php');
+require_once(LIB_PATH_INC.'session.php');
+//require_once(LIB_PATH_INC.'upload.php');
+require_once(LIB_PATH_INC.'database.php');
+require_once(LIB_PATH_INC.'sql.php');
+
+?>
