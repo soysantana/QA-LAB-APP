83db74d5a975a08e40790adf025967168d38348c
Santana <arturo.santtana@gmail.com>
2024-04-23 15:22:24 -0400

    🐛 fix: syntax errors

--- user/auth.php
+++ user/auth.php
@@ -12,7 +12,7 @@ if(empty($errors)){
      $session->login($user_id);
     //Update Sign in time
      updateLastLogIn($user_id);
-     $session->msg("s", "Bienvenido a QA Lab Soil Test Index.");
+     $session->msg("s", "Bienvenido a Laboratorio Mecanica de suelo.");
      redirect('../pages/home.php',false);
 
   } else {
