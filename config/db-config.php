01402187a7a1e3389952ae02815ab0180fab5ee8
Santana <arturo.santtana@gmail.com>
2024-08-29 12:42:02 -0400

    🐛 fix: improvement

--- config/db-config.php
+++ config/db-config.php
@@ -1,8 +1,8 @@
 <?php
 
-  define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_HOST', 'pvj-postgresql.postgres.database.azure.com' );          // Set database host
+  define( 'DB_USER', 'pvj_root');             // Set database user
+  define( 'DB_PASS', 'Laboratorio@' );             // Set database password
   define( 'DB_NAME', 'pvj2_db' );        // Set database name
 
 ?>
