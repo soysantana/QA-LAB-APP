126b4a9611ecd13074b76f1488cf7ebd28de1016
Santana <arturo.santtana@gmail.com>
2025-04-28 16:02:46 -0400

    ⚡ add: corrected for the day

--- pages/grain-size-menu.php
+++ pages/grain-size-menu.php
@@ -1,6 +1,6 @@
 <?php
-  $page_title = 'Grain Size Menu';
-  require_once('../config/load.php');
+$page_title = 'Grain Size Menu';
+require_once('../config/load.php');
 ?>
 
 <?php page_require_level(2); ?>
@@ -8,41 +8,42 @@
 
 <main id="main" class="main">
 
-<div class="pagetitle">
-  <h1>Grain Size</h1>
-  <nav>
-    <ol class="breadcrumb">
-      <li class="breadcrumb-item"><a href="home.php">Home</a></li>
-      <li class="breadcrumb-item">Pages</li>
-      <li class="breadcrumb-item active">Grain Size</li>
-    </ol>
-  </nav>
-</div><!-- End Page Title -->
+  <div class="pagetitle">
+    <h1>Grain Size</h1>
+    <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="home.php">Home</a></li>
+        <li class="breadcrumb-item">Pages</li>
+        <li class="breadcrumb-item active">Grain Size</li>
+      </ol>
+    </nav>
+  </div><!-- End Page Title -->
 
-<section class="section">
-  <div class="row">
-    <div class="col-lg-12">
+  <section class="section">
+    <div class="row">
+      <div class="col-lg-12">
 
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Tipos de ensayos</h5>
-          
-          <div class="list-group">
-            <a href="grain-size.php" class="list-group-item list-group-item-action">General</a>
-            <a href="grain-size-coarse-filter.php" class="list-group-item list-group-item-action">CF</a>
-            <a href="grain-size-fine-filter.php" class="list-group-item list-group-item-action">FF</a>
-            <a href="grain-size-lpf.php" class="list-group-item list-group-item-action">LPF</a>
-            <a href="grain-size-upstream-transition-fill.php" class="list-group-item list-group-item-action">UTF</a>
-            <a href="grain-size-trf.php" class="list-group-item list-group-item-action">TRF</a>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Tipos de ensayos</h5>
+
+            <div class="list-group">
+              <a href="grain-size.php" class="list-group-item list-group-item-action">General</a>
+              <a href="grain-size-coarse-filter.php" class="list-group-item list-group-item-action">CF</a>
+              <a href="grain-size-fine-filter.php" class="list-group-item list-group-item-action">FF</a>
+              <a href="grain-size-lpf.php" class="list-group-item list-group-item-action">LPF</a>
+              <a href="grain-size-upstream-transition-fill.php" class="list-group-item list-group-item-action">UTF</a>
+              <a href="grain-size-trf.php" class="list-group-item list-group-item-action">TRF</a>
+              <a href="hydrometer.php" class="list-group-item list-group-item-action">Hydrometer</a>
+            </div>
+
           </div>
-
         </div>
+
       </div>
 
     </div>
-    
-  </div>
-</section>
+  </section>
 
 </main><!-- End #main -->
 
--- pages/test-preparation.php
+++ pages/test-preparation.php
@@ -100,7 +100,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
           <div class="card-body">
             <h5 class="card-title">LISTA DE MUESTRAS EN PREPARACIÓN</h5>
 
-            <?php $week = date('Y-m-d', strtotime('-8 days')); ?>
+            <?php $week = date('Y-m-d', strtotime('-21 days')); ?>
             <?php $realization = "(SELECT 1 FROM test_realization WHERE sample_name = p.sample_name AND sample_number = p.sample_number AND test_type = p.test_type)"; ?>
             <?php $Seach = find_by_sql("SELECT * FROM test_preparation p WHERE Start_Date >= '{$week}' AND NOT EXISTS $realization ORDER BY Register_Date DESC"); ?>
 
