b342505c7076a9e852ba44a1e28ec0d4ea7b6dde
Santana <arturo.santtana@gmail.com>
2025-05-29 09:55:50 -0400

    🐛 fix: change the logic

--- js/grain-size/gs-cf.js
+++ js/grain-size/gs-cf.js
@@ -348,9 +348,9 @@
         return "GP-Poorly graded gravel";
     } else if (gravel > sand && fines < 5 && Cu < 4 &&  Cc > 3 && sand < 15) {
         return "GP-Poorly graded gravel";
-    } else if (gravel > sand && fines < 5 && Cu < 4 && Cc < 1 && Cc > 3 && sand >= 15) {
+    } else if (gravel > sand && fines < 5 && Cu < 4 && Cc >= 1 && Cc <= 3 && sand >= 15) {
         return "GP-Poorly graded gravel with sand";
-    } else if (gravel > sand && fines < 5 && Cu < 4 &&  Cc > 3 && sand >= 15) {
+    } else if (gravel > sand && fines < 5 && Cu < 4 && Cc > 3 && sand >= 15) {
         return "GP-Poorly graded gravel with sand";
     } else if (gravel > sand && fines >= 5 && fines <= 12 && Cu >= 4 && Cc >= 1 && Cc <= 3 && sand < 15) {
         return "GW-GM Well graded gravel with silt";
