956d41c26e261504080ff8f04a10bcf03a34d74e
Wenjos <josedin@hotmail.com>
2025-08-12 09:29:24 -0400

    Cliente-10

--- pages/detalle-cliente.php
+++ pages/detalle-cliente.php
@@ -343,7 +343,7 @@ function monthName($m) {
                               <?php endif; ?>
                             </div>
                           </div>
-                        </div>
+                        </div>  
                         <div class="modal-footer">
                           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
                         </div>
