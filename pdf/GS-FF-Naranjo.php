07db6b31afea05a44e9237b2a3b385f016eb3852
Santana <arturo.santtana@gmail.com>
2025-09-10 16:04:42 -0400

    update gs-naranjo

--- pdf/GS-FF-Naranjo.php
+++ pdf/GS-FF-Naranjo.php
@@ -387,9 +387,9 @@ $pdf->Cell(152, 4, $resultado, 0, 1, 'C');
 
 // Comments and Observations
 $pdf->SetXY(54, 480);
-$pdf->Cell(360, 4, $Search['Comments'], 0, 1, 'L');
+$pdf->MultiCell(360, 4, $Search['Comments'], 0, 'L');
 $pdf->SetXY(54, 520);
-$pdf->Cell(360, 4, $Search['FieldComment'], 0, 1, 'L');
+$pdf->MultiCell(360, 4, $Search['FieldComment'], 0, 'L');
 
 // Function to insert base64 image into PDF
 function insertarImagenBase64($pdf, $base64Str, $x, $y, $w, $h)
