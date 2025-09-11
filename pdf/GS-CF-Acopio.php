a7e02ddf8fc5dc41b5d8820433dceb5ae29e4e07
Santana <arturo.santtana@gmail.com>
2025-09-11 08:19:45 -0400

    ✨ feat: pdf template and reported

--- pdf/GS-CF-Acopio.php
+++ pdf/GS-CF-Acopio.php
@@ -383,10 +383,10 @@ $pdf->Cell(152, 4, $resultado, 0, 1, 'C');
 
 // Comments and Observations
 $pdf->SetXY(54, 425);
-$pdf->Cell(360, 4, $Search['Comments'], 0, 1, 'L');
+$pdf->MultiCell(360, 4, $Search['Comments'], 0, 'L');
 
 $pdf->SetXY(54, 470);
-$pdf->Cell(360, 4, $Search['FieldComment'], 0, 1, 'L');
+$pdf->MultiCell(360, 4, $Search['FieldComment'], 0, 'L');
 
 // Function to insert base64 image into PDF
 function insertarImagenBase64($pdf, $base64Str, $x, $y, $w, $h)
