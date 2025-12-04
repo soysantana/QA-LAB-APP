0c2392e498a5d091f88593877e8b4a02e4d3c5f2
Wenjos <josedin@hotmail.com>
2025-12-04 15:46:16 -0400

    Revision

--- pages/generar_reporte_pdf.php
+++ pages/generar_reporte_pdf.php
@@ -403,7 +403,7 @@ function draw_client_bar_chart($pdf, array $clientes) {
 function render_ensayos_reporte($pdf, $start, $end) {
 
     // Filtrar SOLO las estructuras permitidas
-    $allowed_structures = "'LLD','SD1','SD2','SD3','PVDJ-AGG',,'LBOR','PVDJ-AGG-INV'";
+    $allowed_structures = "'LLD-258', 'LLD-265','SD1-258','SD2-258','SD3-258','PVDJ-AGG',,'LBOR26','LBOR25','PVDJ-AGG-INV'";
 
     // Obtener datos filtrados
     $ensayos_reporte = find_by_sql("
