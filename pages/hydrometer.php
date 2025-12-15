9df36670b086b9857a5863aca79042b166efd79f
Wenjos <josedin@hotmail.com>
2025-12-15 13:17:54 -0400

    pendiente

--- pages/hydrometer.php
+++ pages/hydrometer.php
@@ -366,7 +366,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                             </table>
 
 
-                        </div>
+                        </div>  
                     </div>
 
                 </div>
