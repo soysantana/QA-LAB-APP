96fe87c33388c02e3dd7862d34c9834756112d60
Santana <arturo.santtana@gmail.com>
2025-12-17 10:02:25 -0400

    test v2

--- user/auth.php
+++ user/auth.php
@@ -1,36 +1,27 @@
-<?php
-// DEBUG TEMPORAL (quitar luego)
-file_put_contents(__DIR__ . "/auth_hit.log", date("c") . " HIT auth.php\n", FILE_APPEND);
-header("X-AUTH-HIT: 1");
-?>
 <?php include_once('../config/load.php'); ?>
 
-
-<?php include_once('../config/load.php'); ?>
 <?php
-$req_fields = array('username','password' );
+$req_fields = array('username', 'password');
 validate_fields($req_fields);
 $username = remove_junk($_POST['username']);
 $password = remove_junk($_POST['password']);
 
-if(empty($errors)){
+if (empty($errors)) {
   $user_id = authenticate($username, $password);
-  if($user_id){
+  if ($user_id) {
     //create session with id
-     $session->login($user_id);
+    $session->login($user_id);
     //Update Sign in time
-     updateLastLogIn($user_id);
-     $session->msg("s", "Bienvenido a Laboratorio Mecanica de suelo.");
-     redirect('../pages/home.php',false);
-
+    updateLastLogIn($user_id);
+    $session->msg("s", "Bienvenido a Laboratorio Mecanica de suelo.");
+    redirect('../pages/home.php', false);
   } else {
     $session->msg("d", "Nombre de usuario y/o contraseña incorrecto.");
-    redirect('../index.php',false);
+    redirect('../index.php', false);
   }
-
 } else {
-   $session->msg("w", $errors);
-   redirect('../index.php',false);
+  $session->msg("w", $errors);
+  redirect('../index.php', false);
 }
 
 ?>
